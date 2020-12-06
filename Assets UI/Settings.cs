646bba0a366485f656bd3aa88ff8a56a05fa78d7
Kushagra Gupta <kushg30@gmail.com>
2020-12-06 19:20:02 +0530

    llul

---
+++ Assets UI/Settings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Settings : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void setVolume(float volume){
+
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex){
+
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen){
+        Screen.fullScreen = isFullscreen;
+    }
+}
